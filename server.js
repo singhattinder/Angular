cd1579ffe15967364e278158d8ff400e8e7599a3
Attinderpal <singh.at@husky.neu.edu>
2018-06-19 22:44:52 -0400

    heroku f5

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/app'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/app/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/wedev-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
