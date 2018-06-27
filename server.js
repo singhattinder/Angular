37e994ba896f49b7c43e3a1bb7815e0af1cf6ee4
Attinderpal <singh.at@husky.neu.edu>
2018-06-27 18:29:19 -0400

    heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/*', function(req,res) {
 });
 
 // Start the app by listening on the default Heroku port
-var server = app.listen(process.env.PORT || 4005, function () {
+var server = app.listen(process.env.PORT || 4000, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
